3072a1bf0200bd4256eca8cd982ce69474b62ba1
ros1986 <rosuzaini86@yahoo.com>
2026-01-30 20:08:39 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect
+import pyodbc
+
+app = Flask(__name__)
+
+conn = pyodbc.connect(
+    "DRIVER={ODBC Driver 18 for SQL Server};"
+    "SERVER=rosuzaini.database.windows.net;"
+    "DATABASE=FAcloud;"
+    "UID=rosuzaini;"
+    "PWD=admin@1234;"
+)
+
+@app.route("/", methods=["GET", "POST"])
+def form():
+    if request.method == "POST":
+        student_id = request.form["student_id"]
+        attendance = request.form["attendance"]
+        quiz = request.form["quiz"]
+
+        cursor = conn.cursor()
+        cursor.execute("""
+            INSERT INTO Performance (StudentID, Attendance, QuizScore)
+            VALUES (?, ?, ?)
+        """, student_id, attendance, quiz)
+        conn.commit()
+
+        return redirect("/")
+
+    return render_template("form.html")
+
+if __name__ == "__main__":
+    app.run()
